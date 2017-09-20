07e598f1beeac207a2555973a198823fde87b345
Islam Wazery <wazery@ubuntu.com>
2017-09-20 20:28:57 +0200

    Add support for indifferent access for the configuration keys

--- lib/horus/api.rb
+++ lib/horus/api.rb
@@ -1,12 +1,16 @@
+require 'active_support/core_ext/hash/indifferent_access'
+
 module Horus
   class API
     attr_accessor :api_key, :default_language, :default_country_code, :default_units
 
     def initialize(options = {})
-      @api_key = options[:api_key] || options['api_key']
-      @default_language = options[:default_language] || options['default_language'] || 'en'
-      @default_country_code = options[:default_country_code] || options['default_country_code']
-      @default_units =  options[:default_units] || options['default_units'] || 'metric'
+      options = options.with_indifferent_access
+
+      @api_key = options[:api_key]
+      @default_language = options[:default_language] || 'en'
+      @default_country_code = options[:default_country_code]
+      @default_units =  options[:default_units] || 'metric'
     end
 
     def current(**args, &block)
